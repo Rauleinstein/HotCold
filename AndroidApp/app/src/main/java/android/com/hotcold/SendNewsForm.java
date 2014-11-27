50ebe3357289e9e507b805dc8d326ee570a44287
bott17 <bott17@gmail.com>
2014-11-27 18:14:49 +0100

    Capture a photo, alpha

--- AndroidApp/app/src/main/java/android/com/hotcold/SendNewsForm.java
+++ AndroidApp/app/src/main/java/android/com/hotcold/SendNewsForm.java
@@ -2,13 +2,24 @@ package android.com.hotcold;
 
 import android.app.Activity;
 import android.com.hotcold.androidapp1.R;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import network.Backend;
 
@@ -19,6 +30,12 @@ public class SendNewsForm extends Activity {
     Button sendNewsButton;
     ImageView imgPreview;
     EditText textNew;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
+
+    String imageNewsFileName; //Name of the image to will be send
 
 
     @Override
@@ -70,6 +87,8 @@ public class SendNewsForm extends Activity {
             @Override
             public void onClick(View v) {
                 //TODO añadir funcionalidad
+                dispatchTakePictureIntent();
+
             }
         });
 
@@ -77,5 +96,57 @@ public class SendNewsForm extends Activity {
 
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Capture the intent of camera, and set it to imgage preview
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgPreview.setImageBitmap(imageBitmap);
+        }
+    }
+
+    // Launch camera to get a picture
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("SendNewsForm", "Fail to create the file for photo");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    } // TODO Falta introducir la foto en la galeria. Documentacion en android developers
+
+    // Create a file for the full img capture from camera
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        imageNewsFileName = imageFileName;
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
 
 }
